2ac26a2e534449c252a0c8f8c19aae43af403832
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-12-03 16:42:50 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="mt-5">Hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
