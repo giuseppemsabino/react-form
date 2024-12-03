c86b1dca2da3ec238ef94d6c33a2a6e2343ed17f
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-12-03 17:09:11 +0100

    input and handleres declared

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 
 function App() {
  
+  const [myValue , setMyValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setMyValue(e.target.value);
+  }
+
+  const handleFormSubmit = (e) => {
+
+  }
 
   return (
     <>
       <div className="container">
         <h1 className="mt-5">Hello world</h1>
+        <form onSubmit={handleFormSubmit}>
+          <input type="text" value={myValue} onChange={handleInputChange} />
+          <button>Invia</button>
+        </form>
       </div>
     </>
   )
